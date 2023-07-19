6bf2194a0455e8f84f3079412efedb858191c62e
Mark Ortega <mortegaponce27@gmail.com>
2023-07-18 21:37:42 -0500

    repository/StudentRepository.java: Extending JpaRepository in interface to use object-relation mappings between model and database. (Django-esque)

--- studentsystem/src/main/java/com/example/studentsystem/repository/StudentRepository.java
+++ studentsystem/src/main/java/com/example/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,12 @@
 // For spring data japa implementation
 package com.example.studentsystem.repository;
-public interface StudentRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+// Give name of model class, and type of the primary key.
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
 }
