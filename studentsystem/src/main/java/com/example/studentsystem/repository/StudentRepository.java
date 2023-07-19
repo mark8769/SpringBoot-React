5e5a86ce71d229fb3f7bdac5eb13c04f476676ee
Mark Ortega <mortegaponce27@gmail.com>
2023-07-18 22:50:25 -0500

    Added mysql properties to application.properties files. Fixed maven dependency for model/Student.java.

--- studentsystem/src/main/java/com/example/studentsystem/repository/StudentRepository.java
+++ studentsystem/src/main/java/com/example/studentsystem/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 // For spring data japa implementation
 package com.example.studentsystem.repository;
 
+import com.example.studentsystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
@@ -1,42 +1,4 @@
 package com.example.studentsystem.service;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class StudentServiceImplementation {
-    @Id // Makes it the primary key
-    @GeneratedValue(strategy= GenerationType.IDENTITY) // Make id autoincrement
-    private int id;
-    private String name;
-    private String address;
-
-    public StudentServiceImplementation() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
