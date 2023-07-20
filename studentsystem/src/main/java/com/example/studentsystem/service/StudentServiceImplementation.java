0ce4d416dd477ceac4fdf96c7dfabb5c5b5659ad
Mark Ortega <mortegaponce27@gmail.com>
2023-07-19 23:05:46 -0500

    service/StudentServiceImplementation: Overrided getAllStudents method from StudentService interface.

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
@@ -5,6 +5,8 @@ import com.example.studentsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImplementation implements StudentService{
 
@@ -18,5 +20,10 @@ public class StudentServiceImplementation implements StudentService{
     @Override
     public Student saveStudent(Student student){
         return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
     }
 }
