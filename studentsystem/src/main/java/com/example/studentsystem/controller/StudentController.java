5c4182e3da0f4937c4d532794558e1a07982039c
Mark Ortega <mortegaponce27@gmail.com>
2023-07-19 23:04:09 -0500

    controller/StudentController: Added GetMapping for retrieving all students.

--- studentsystem/src/main/java/com/example/studentsystem/controller/StudentController.java
+++ studentsystem/src/main/java/com/example/studentsystem/controller/StudentController.java
@@ -4,10 +4,9 @@ package com.example.studentsystem.controller;
 import com.example.studentsystem.model.Student;
 import com.example.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -23,5 +22,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student is added.";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
