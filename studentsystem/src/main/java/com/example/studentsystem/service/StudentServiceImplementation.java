29bb40907f37f88176f0bff2993766efb03624b6
Mark Ortega <mortegaponce27@gmail.com>
2023-07-19 20:21:13 -0500

    service/StudentServiceImplementation: Added dependency injecton to call studentRepository. Implemented methods from StudentService interface.:

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImplementation.java
@@ -1,4 +1,22 @@
 package com.example.studentsystem.service;
 
-public class StudentServiceImplementation {
+import com.example.studentsystem.model.Student;
+import com.example.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImplementation implements StudentService{
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImplementation(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student saveStudent(Student student){
+        return studentRepository.save(student);
+    }
 }
