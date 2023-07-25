56522ace525ccd602a15636a6414ecfd5c1e6cef
Mark Ortega <mortegaponce27@gmail.com>
2023-07-24 21:52:44 -0500

    studentFrontend/src/components/Student.js: Added state management to student form component.

--- studentsystem/studentfrontend/src/components/Student.js
+++ studentsystem/studentfrontend/src/components/Student.js
@@ -1,7 +1,8 @@
 // Student.js: Component
 // Adding student form
 
-import * as React from 'react';
+// import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container'
@@ -10,14 +11,22 @@ import Paper from '@mui/material/Paper'
 export default function Student() {
     
     const paperStyle = {padding: '50px 20px', width: 600, margin:"20px auto"}
+    const[nameVar, setName]=useState("") // State management for name field
+    const[addressVar, setAddress]=useState("") // State management for address field
 
     return (
     <Container>
         <Paper elevation={3} style={paperStyle}>
             <h1 style={{color: "blue"}}><u>Add Student</u></h1>
             <Box component="form" sx={{'& > :not(style)': { m: 1},}} noValidate autoComplete="off">
-            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth/>
-            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth/>
+            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+            value={nameVar} // Using var declared in const
+            onChange={(e) => setName(e.target.value)} // when we write something (event), save text into nameVar
+            />
+            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+            value={addressVar}
+            onChange={(e) => setName(e.target.value)}
+            />
             </Box>
         </Paper>
     </Container>
