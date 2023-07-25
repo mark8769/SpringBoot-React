2adee028652ef3e47be457a9b83cd356c135a5cc
Mark Ortega <mortegaponce27@gmail.com>
2023-07-24 21:45:14 -0500

    studentFrontend/src/components/Student.js: Added student component form in components. Added some styling too. Kinda weird, but just have to learn.

---
+++ studentsystem/studentfrontend/src/components/Student.js
@@ -0,0 +1,25 @@
+// Student.js: Component
+// Adding student form
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper'
+
+export default function Student() {
+    
+    const paperStyle = {padding: '50px 20px', width: 600, margin:"20px auto"}
+
+    return (
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
+            <h1 style={{color: "blue"}}><u>Add Student</u></h1>
+            <Box component="form" sx={{'& > :not(style)': { m: 1},}} noValidate autoComplete="off">
+            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth/>
+            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth/>
+            </Box>
+        </Paper>
+    </Container>
+    );
+}
